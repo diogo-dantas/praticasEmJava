557f49e73d30f3f4fe73522567e36f023220d097
Diogo Dantas <diogo.dantas@live.com>
2022-02-05 23:05:43 -0300

    Convertendo interfaces em lambdas.

--- javaDiogo/test/TesteDefaultMethods.java
+++ javaDiogo/test/TesteDefaultMethods.java
@@ -1,9 +1,7 @@
 package br.com.git.javaDiogo.test;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class TesteDefaultMethods {
 
@@ -21,44 +19,15 @@ public class TesteDefaultMethods {
 // melhor prática aplicando um comparator ao método da lista cursos
 // alterado a classificação por tamanho da string.
 
-		Comparator<String> comparador = new comparaPorTamanho();
-		cursos.sort(comparador);
+		cursos.sort((s, s2) -> Integer.compare(s.length(), s2.length()));
 
 //		for (String c : cursos) {
 //			System.out.println(c);
 //		}
 // melhor prática aplicando consumer ao método de cursos...
 
-		Consumer<String> curso = new imprimeLinha();
-		cursos.forEach(curso);
+		cursos.forEach(s -> System.out.println(s));
 
 	}
 
-}
-
-
-class imprimeLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-
-	}
-}
-
-
-class comparaPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s, String s2) {
-		if (s.length() > s2.length()) {
-			return 1;
-		}
-		if (s.length() < s2.length()) {
-			return -1;
-		}
-
-		return 0;
-
-	}
 }
